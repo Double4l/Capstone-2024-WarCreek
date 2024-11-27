f707662539619abeb6527e71dedac592bde64e91
Double4l <83231906+Double4l@users.noreply.github.com>
2024-11-27 14:04:31 -0500

    Added Blue Print For Game State Class

--- Source/FPSGame/Private/FPSGameStateBase.cpp
+++ Source/FPSGame/Private/FPSGameStateBase.cpp
@@ -2,4 +2,27 @@
 
 
 #include "FPSGameStateBase.h"
+#include "Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+#include "Net/UnrealNetwork.h"
+
+AFPSGameStateBase::AFPSGameStateBase()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    SetReplicates(true);
+}
+
+void AFPSGameStateBase::BeginPlay()
+{
+    Super::BeginPlay();
+}
+
+void AFPSGameStateBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME(AFPSGameStateBase, PlayerOneLoggedIn);
+    DOREPLIFETIME(AFPSGameStateBase, PlayerTwoLoggedIn);
+    DOREPLIFETIME(AFPSGameStateBase, PlayerOneScore); // sus
+    DOREPLIFETIME(AFPSGameStateBase, PlayerTwoScore); // sus
+}
 
--- Source/FPSGame/Public/FPSGameStateBase.h
+++ Source/FPSGame/Public/FPSGameStateBase.h
@@ -13,5 +13,24 @@ UCLASS()
 class FPSGAME_API AFPSGameStateBase : public AGameStateBase
 {
 	GENERATED_BODY()
+
+public:
+    AFPSGameStateBase();
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
+    int PlayerOneScore;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (ClampMin = 0), Replicated)
+    int PlayerTwoScore;
+
+public:
+    UPROPERTY(EditAnywhere, Replicated)
+    bool PlayerOneLoggedIn;
+
+    UPROPERTY(EditAnywhere, Replicated)
+    bool PlayerTwoLoggedIn;
+
+protected:
+    virtual void BeginPlay() override;
 	
 };
